859ab35d89a5f2c572e0f7da144c655b0368bc00
eldiiar <a.eldiyar09@gmail.com>
2020-12-11 08:04:47 +0600

    updated models for unv

--- core/university/admin.py
+++ core/university/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import University, Gallery
+from .models import University, Degree, Faculty, FinanceAid, FAQ,  Gallery
 
 
 @admin.register(University)
@@ -7,7 +7,26 @@ class UniversityAdmin(admin.ModelAdmin):
     pass
 
 
+@admin.register(Degree)
+class DegreeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Faculty)
+class FacultyAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(FinanceAid)
+class FinanceAidAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(FAQ)
+class FAQAdmin(admin.ModelAdmin):
+    pass
+
+
 @admin.register(Gallery)
 class GalleryAdmin(admin.ModelAdmin):
     pass
-# Register your models here.
--- core/university/models.py
+++ core/university/models.py
@@ -11,7 +11,36 @@ class University(models.Model):
         return reverse('university:details', kwargs={'pk': self.pk})
 
 
+class Degree(models.Model):
+    university = models.ForeignKey('University', on_delete=models.CASCADE, related_name='degrees')
+    qualifications = models.CharField(max_length=50)
+    information = models.TextField()
+
+
+class Faculty(models.Model):
+    degree = models.ForeignKey('Degree', on_delete=models.CASCADE, related_name='faculties')
+    title = models.CharField(max_length=100)
+    LANGUAGES_CHOICES = (('KG', 'Kyrgyz'), ('RU', 'Russian'), ('EN', 'English'), ('TU', 'Turkish'))
+    language = models.CharField(max_length=2, choices=LANGUAGES_CHOICES, default='Russian')
+    description = models.TextField()
+    duration = models.IntegerField(default=4)
+    fee = models.IntegerField()
+
+
+class FinanceAid(models.Model):
+    university = models.ForeignKey('University', on_delete=models.CASCADE, related_name='aid')
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+
+
+class FAQ(models.Model):
+    university = models.ForeignKey('University', on_delete=models.CASCADE, related_name='faq')
+    title = models.CharField(max_length=100, default='Frequently asked Question')
+    description = models.TextField()
+
+
 class Gallery(models.Model):
-    image = models.ImageField(upload_to='university_img')
     university = models.ForeignKey('University', on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='university_img')
+
 
